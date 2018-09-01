7bf28ccad9c5e9fbff779465521437a8a88beaba
caitaozhan <caitaozhan@gmail.com>
2018-08-31 20:50:44 -0400

    memory profile

---
+++ profile/mem_profile.py
@@ -0,0 +1,28 @@
+from scipy.stats import multivariate_normal
+import numpy as np
+
+@profile
+def my_func():
+    a = [1] * (10**6)
+    b = [2] * (2*10**7)
+    del b
+    return a
+
+@profile
+def multivariate():
+    mymean = np.random.randn(1000)
+    mycov = np.zeros(1000000).reshape(1000, 1000)
+    for i in range(1000):
+        mycov[i, i] = 1
+    mylist = []
+    for _ in range(100):
+        mylist.append(multivariate_normal(mean=mymean, cov=mycov))
+    del mylist
+
+
+if __name__ == '__main__':
+    my_func()
+    my_func()
+    multivariate()
+
+# in command line: python -m memory_profiler mem_profile.py
