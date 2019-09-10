b8f3efc9796840d6a1d14887b5867358d63a0bbd
caitaozhan <caitaozhan@gmail.com>
2019-09-10 16:41:10 -0400

    curl + app.run() single process mode done. next step is to incoporate gunicorn and nginx

--- flask/hello.py
+++ flask/hello.py
@@ -1,17 +1,18 @@
-from flask import Flask, redirect, url_for, request, jsonify
 import time
+from flask import Flask, redirect, url_for, request, jsonify, json
+from task import task
 
 app = Flask(__name__)
 
 
 @app.route('/admin')
 def hello_admin():
-    return 'Hello Admin'
+    return 'Hello Admin\n'
 
 
 @app.route('/guest/<guest>')
 def hello_guest(guest):
-    return 'Hello {} as Guest'.format(guest)
+    return 'Hello {} as Guest\n'.format(guest)
 
 
 @app.route('/user/<name>')
@@ -22,10 +23,15 @@ def hello_user(name):
         return redirect(url_for('hello_guest', guest=name))
 
 
-@app.route('/foo', methods=['POST'])
-def foo():
-    data = request.json
-    print(jsonify(data))
+#  curl -d '{"three":"three"}' -H 'Content-Type: application/json' -X POST http://localhost:5000/localize
+@app.route('/localize', methods=['POST'])
+def localize():
+    data = request.get_json()   # type(data) = dict
+    print(list(data.values())[0], end=' ')
+    mysum = task()
+    data['sum'] = mysum
+    return str(data) + '\n'
+
 
 
 if __name__ == '__main__':
