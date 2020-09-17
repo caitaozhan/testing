ed8ecc87322635db517c7f700fc8b8ff0c4825d2
caitaozhan <caitaozhan@gmail.com>
2020-09-17 12:35:01 -0400

    nested function

--- nested.py
+++ nested.py
@@ -1,3 +1,6 @@
+'''a nested function needs to be defined before calling it
+'''
+
 class Utility:
     def outer(self, outer: str):
         def inner():
@@ -11,13 +14,15 @@ class Utility2:
     @staticmethod
     def outer(outer: str):
         def inner():
-            print(outer*2)
             print('inner')
+            memo[1] = 2
+            print(outer*2)
         print(outer*4)
+        memo = {}
         inner()
-
-u = Utility()
-u.outer('o')
+        print(memo)
+# u = Utility()
+# u.outer('o')
 
 u2 = Utility2()
 u2.outer('o')
