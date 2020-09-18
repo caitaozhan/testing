11ce7db52ab609e78fd5ffdaa49f562dcc6515d3
caitaozhan <caitaozhan@gmail.com>
2020-09-18 17:34:47 -0400

    greedy maximum weight matching in Python

--- maximum-weight-matching/linear_sum_assignment.py
+++ maximum-weight-matching/linear_sum_assignment.py
@@ -3,7 +3,10 @@
 
 import time
 import random
+import numpy as np
+from greedy_match import cost2edges, greedy
 from ortools.graph import pywrapgraph
+
 
 
 def create_data_array_toy():
@@ -35,22 +38,24 @@ def main(cost):
     for worker in range(rows):
         for task in range(cols):
             if cost[worker][task]:
-                assignment.AddArcWithCost(worker, task, cost[worker][task])
+                assignment.AddArcWithCost(worker, task, int(cost[worker][task]))
     print('2. time for initializing the linear assignment solver = {:.6f}'.format(time.time()-start))
 
     # 3: invoke the solver
+    opt = 0
     start = time.time()
     solve_status = assignment.Solve()
     print('3. time for the algorithm = {:.6f}'.format(time.time() - start))
     if solve_status == assignment.OPTIMAL:
-        print('Total cost = ', assignment.OptimalCost(), '\n')
+        opt = assignment.OptimalCost()
+        print('Total cost = ', opt, '\n')
         # for i in range(assignment.NumNodes()):
         #     print('worker {} assigned to task {}. cost = {}'.format(i, assignment.RightMate(i), assignment.AssignmentCost(i)))
     elif solve_status == assignment.INFEASIBLE:
         print('No assignment is possible')
     elif solve_status == assignment.POSSIBLE_OVERFLOW:
         print('Some inputs costs are too large and may cause an integer overflow')
-
+    return opt
 
 
 if __name__ == '__main__':
@@ -59,4 +64,14 @@ if __name__ == '__main__':
         start = time.time()
         cost = create_data_array(n)
         print('1. time for creating the cost matrix = {:.6f}'.format(time.time()-start))
-        main(cost)
+        opt = main(cost) * -1
+
+        edges = cost2edges(cost)
+        weight_sum, _ = greedy(n, edges)
+        print('weight sum', weight_sum)
+        print('{} percent optimal'.format(weight_sum/opt*100))
+        print('*'*10, '\n')
+
+    # cost = create_data_array_toy()
+    # cost = -np.array(cost).astype(np.int)
+    # main(cost)
